fa119f98723049f74f42cdc75f06890594a65cb9
brikehn <thebriankwon@gmail.com>
2022-03-24 11:57:15 -0400

    fix

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -1,12 +1,28 @@
 import { REST } from "@discordjs/rest";
-import { Routes } from "discord-api-types/v9";
+import { APIApplicationCommandOption, Routes } from "discord-api-types/v9";
 import { Client } from "discord.js";
 import { CommandList } from "../commands/_CommandList";
 
 export const onReady = async (client: Client) => {
   const rest = new REST({ version: "9" }).setToken(process.env.TOKEN as string);
 
-  const commandData = CommandList.map((command) => command.data.toJSON());
+  const commandData: {
+    name: string;
+    description?: string;
+    type?: number;
+    options?: APIApplicationCommandOption[];
+  }[] = [];
+
+  CommandList.forEach((command) =>
+    commandData.push(
+      command.data.toJSON() as {
+        name: string;
+        description?: string;
+        type?: number;
+        options?: APIApplicationCommandOption[];
+      }
+    )
+  );
 
   await rest.put(
     Routes.applicationGuildCommands(
